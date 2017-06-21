3b96de10b2e14b58cc6d79cf50f0bd7124b1932f
Lucas Overland <loverland@linkedin.com>
2017-06-21 00:27:28 -0700

    color theory is crazy

--- src/ui/components/color-sort/component.ts
+++ src/ui/components/color-sort/component.ts
@@ -1,5 +1,24 @@
-import Component from "@glimmer/component";
+import Component, { tracked } from "@glimmer/component";
 
 export default class ColorSort extends Component {
+
+  @tracked items: any[] = [];
+  @tracked itemsOfItems: any[] = [];
+
+  constructor(options) {
+    super(options);
+    let temp = [];
+    for (var c = 1; c < 100; c++) {
+      temp[c % 5] = `#${('000000' + (~~(Math.random() * 16777215)).toString(16)).slice(-6)}`
+      if (c % 5 === 0) {
+        this.itemsOfItems.push({
+          even: (this.itemsOfItems.length + 1) % 2 === 0,
+          colors: temp
+        });
+        temp = [];
+      }
+    }
+    console.log(this.items)
+  }
 
 }
